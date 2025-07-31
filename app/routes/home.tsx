53c99ce754f701d1211f4f8ea4e4c5a6619905fc
Ankit <goswamiankit2110@gmail.com>
2025-07-31 18:20:37 +0530

    first commit

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -44,11 +44,12 @@ export default function Home() {
 
     <section className="main-section">
       <div className="page-heading py-16">
-        <h1>Track Your Applications & Resume Ratings</h1>
+        <h1>Resume Ratings</h1>
         {!loadingResumes && resumes?.length === 0 ? (
             <h2>No resumes found. Upload your first resume to get feedback.</h2>
         ): (
-          <h2>Review your submissions and check AI-powered feedback.</h2>
+          // <h2>Review your submissions and check AI-powered feedback.</h2>
+          <h2>Job Description + Resume → Instant AI feedback to boost your match rate</h2>
         )}
       </div>
       {loadingResumes && (
